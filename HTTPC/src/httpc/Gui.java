b2567a2bab87057a55fc0569dbb46c737362db22
FisitronPassDynamics <gonzalesandrey748@gmail.com>
2025-09-11 15:15:38 +0200

    added visible status

--- HTTPC/src/httpc/Gui.java
+++ HTTPC/src/httpc/Gui.java
@@ -5,6 +5,7 @@
 package httpc;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -37,20 +38,19 @@ public class Gui extends JFrame {
     public void setAccensioneRele(String accensioneRele) {
         this.accensioneRele.setText(accensioneRele);
     }
-
+    
     public String getNumRele() {
         return numRele.getText();
     }
-
+    
     public void setNumRele(String numRele) {
         this.numRele.setText(numRele);
     }
     
-    
-    
     String urlGet = "http://10.100.0.77/get_rele_status";
     String urlPost = "http://10.100.0.77/set_rele";
-    String command = "RELE[1][0]";
+    
+    
     public Gui() {
         super("Test HttpClient");
         Container frmContentPane = this.getContentPane();
@@ -63,19 +63,37 @@ public class Gui extends JFrame {
                                  Risposta body: x
                                  Versione: x""");
         areaResponse = new JScrollPane(response);
+        relay1 = new JButton();
+        relay2 = new JButton();
+        relay3 = new JButton();
+        relay4 = new JButton();
+        relay5 = new JButton();
+        relay6 = new JButton();
+        
+        JPanel relays = new JPanel();
+        relays.add(relay1); relays.add(relay2); relays.add(relay3); 
+        relays.add(relay4); relays.add(relay5); relays.add(relay6); 
+        
         JPanel input = new JPanel();
         input.setLayout(new GridLayout(3, 1));
         JPanel output = new JPanel();
         output.setLayout(new BorderLayout());
         
+        JPanel inputPanel = new JPanel(new BorderLayout());
         JPanel inputRele = new JPanel();
         numRele = new JTextField("Inserisci numero del Rele [1-6]");
         accensioneRele = new JTextField("Inserisci 1 per accenderlo, 0 spengerlo");
           
         inputRele.add(numRele); inputRele.add(accensioneRele);
         
+        
         input.add(link); input.add(get); input.add(post);
+        
         output.add(areaResponse, BorderLayout.CENTER);
+        output.add(relays, BorderLayout.SOUTH);
+        
+        inputPanel.add(input, BorderLayout.NORTH);
+        inputPanel.add(inputRele, BorderLayout.CENTER);
         
         // FisitronHUB 
         JPanel relePanel = new JPanel(new GridLayout(1, 6, 5, 5));
@@ -90,6 +108,7 @@ public class Gui extends JFrame {
                     String command = String.format("RELE[%d][%d]", releNum, isOn ? 1 : 0);
                     link.setText(urlPost);
                     setData(command);
+                    setColor(getStatus());
                 }
             });
             relePanel.add(releButtons[i]);
@@ -123,7 +142,7 @@ public class Gui extends JFrame {
         
         
         frmContentPane.setLayout(new BorderLayout());
-        frmContentPane.add(input, BorderLayout.NORTH);
+        frmContentPane.add(inputPanel, BorderLayout.NORTH);
         frmContentPane.add(output, BorderLayout.CENTER);
         frmContentPane.add(relePanel, BorderLayout.SOUTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -173,6 +192,7 @@ public class Gui extends JFrame {
         
         return String.format("RELE[%s][%s]", numRele, accensioneRele);
     }
+    
     public void setData(String s){
         String urlString = link.getText().trim();
         if (urlString.isEmpty() || !isValidUrl(urlString)) {
@@ -212,6 +232,38 @@ public class Gui extends JFrame {
             return uri.isAbsolute() && (uri.getScheme().equals("http") || uri.getScheme().equals("https"));
         } catch (Exception e) {
             return false;
+        }
+    }
+    
+    public boolean[] getStatus(){
+        boolean[] status = {false, false, false, false, false, false};
+        String r[] = {"0,0,0,0,0,0"};
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create(urlGet))
+                            .GET()
+                            .version(HttpClient.Version.HTTP_1_1)
+                            .build();
+        try{ 
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            r = response.body().split(",");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Errore");
+        }   
+        for(int i = 0; i < r.length; i++){
+            status[i] = r[i].trim().equals("1");
+        }
+        
+        return status;
+    }
+    public void setColor(boolean[] status){
+        JButton[] btns = {relay1, relay2, relay3, relay4, relay5, relay6};
+        for(int i = 0; i < btns.length; i++){
+            if(status[i])
+                btns[i].setBackground(Color.GREEN);
+            else btns[i].setBackground(Color.red);
         }
     }
 }
