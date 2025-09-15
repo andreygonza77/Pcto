9518ba3eee2230cf209ce30b45477bc8a21c3f3c
FisitronPassDynamics <gonzalesandrey748@gmail.com>
2025-09-15 15:07:05 +0200

    improved the code a bit

--- QT/TesterHUB/mainwindow.cpp
+++ QT/TesterHUB/mainwindow.cpp
@@ -1,14 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QApplication>
+#include <QLineEdit>
+#include <QCheckBox>
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    link = ui->linkEdit;
+    getCheckBox = ui->checkBox;
      //connect(ui->getBtn, &QPushButton::clicked, this, &MainWindow::on_getBtn_clicked);
+
 }
+
+
 
 MainWindow::~MainWindow()
 {
@@ -28,9 +40,27 @@ void MainWindow::on_postBtn_clicked()
 void MainWindow::on_linkEdit_returnPressed()
 {
     QString url = ui->linkEdit->text();
-    qDebug() << "Link: " + url;
-}
+    QString content = ui->contentEdit->text();
+    bool isGetChecked = ui->checkBox->isChecked();
+    bool isPostChecked = ui->checkBox_2->isChecked();
 
+    qDebug() << "URL inserito:" << url;
+    qDebug() << "Contenuto inserito:" << content;
+    qDebug() << "Metodo selezionato:" << (isGetChecked ? "GET" : "POST");
+
+    QString outputText;
+    if(isGetChecked) isPostChecked = false;
+    else isPostChecked = true;
+
+    outputText += "URL: " + url + "\n";
+    outputText += "Contenuto: " + content + "\n";
+    outputText += "Metodo: " + QString(isGetChecked ? "GET" : "POST") + "\n";
+
+
+
+
+    ui->output->setText(outputText);
+}
 void MainWindow::on_checkBox_stateChanged(int arg1)
 {
     if(arg1 == Qt::Checked){
@@ -39,13 +69,5 @@ void MainWindow::on_checkBox_stateChanged(int arg1)
     else if(arg1 == Qt::Unchecked) {
         qDebug() << "Checkbox non selezionato";
     }
-/*
-    if(ui->checkBox->isChecked()){
-        qDebug() << "Checkbox selezionato";
-    }
-    else {
-        qDebug() << "Checkbox non selezionato";
-    }
-*/
-
 }
+
--- QT/TesterHUB/mainwindow.h
+++ QT/TesterHUB/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QCheckBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -27,5 +29,7 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+    QLineEdit *link;
+    QCheckBox *getCheckBox;
 };
 #endif // MAINWINDOW_H
